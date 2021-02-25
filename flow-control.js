70f8c5fbf22d1317b9ebc0206f59a1b6efcc5012
Roxana Sulton <roxsulton@icloud.com>
2021-02-25 17:56:17 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -37,16 +37,17 @@ function ternaryTeenager(age) {
 
 
 function switchAge(age) {
-  if (age > 12 && age < 20) {
-    return "You are a teenager";
-  } else {
-    return "You have an age";
-  }
-  // switch (age) {
-  //   case  (age > 12 && age < 20):
-  //     return "You are a teenager";
-  //   default:
-  //     return "You have an age";
+  // if (age > 12 && age < 20) {
+  //   return "You are a teenager";
+  // } else {
+  //   return "You have an age";
   // }
+  
+  switch (age) {
+    case  (age > 12 && age < 20):
+      return "You are a teenager";
+    default:
+      return "You have an age";
+  }
   // console.log("Tell us your age later.")
 }
