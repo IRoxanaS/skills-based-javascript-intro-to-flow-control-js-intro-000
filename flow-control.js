08153e328b65c5f6b35ebbb377be940a34bebbd4
Roxana Sulton <roxsulton@icloud.com>
2021-02-25 17:23:46 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,54 @@
 function basicTeenager(age) {
-
+  if (age > 12 && age < 20) {
+    return ("You are a teenager!")
+  }
 }
 
+/*
 function teenager(age) {
-
+  if (age > 12 && age < 20) {
+    return "You are a teenager!"
+  } else {
+    return "You are not a teenager."
+  }
 }
+
 
 function ageChecker(age) {
-
+  if (age > 12 && age < 20) {
+    return "You are a teenager!"
+  } else if (age < 13) {
+    return "You are a kid."
+  } else {
+    return "You are a grownup."
+  }
 }
 
+
 function ternaryTeenager(age) {
+if (age > 12 && age < 20) {
+  return "You are a teenager."
+} else if (age < 12) {
+  return "You are a kid"
+} else } (age < 3)}
+return "You are not a teenager."
+}
+
 
 }
 
 function switchAge(age) {
+  switch (age) {
+    case (age is 13-19):
+    return "You are a teenager."
+  default:
+  return "You have an age"
+}
+console.log("Tell us your age later.")
+}
+
 
 }
+
+}
+*/
