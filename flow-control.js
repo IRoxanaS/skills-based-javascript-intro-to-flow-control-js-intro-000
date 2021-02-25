3a291c1c6abe7066f3ab4fb5b304f8898808446d
Roxana Sulton <roxsulton@icloud.com>
2021-02-25 17:55:58 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -38,7 +38,7 @@ function ternaryTeenager(age) {
 
 function switchAge(age) {
   if (age > 12 && age < 20) {
-    return "You are a teenager";  
+    return "You are a teenager";
   } else {
     return "You have an age";
   }
